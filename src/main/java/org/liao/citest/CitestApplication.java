095724bdd3444a68ee02a8c83c9c9d56de3699ac
liaozs <1596501070@qq.com>
2024-05-18 15:46:18 +0800

    文件上传测试

--- src/main/java/org/liao/citest/CitestApplication.java
+++ src/main/java/org/liao/citest/CitestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CitestApplication {
 
     public static void main(String[] args) {
+        System.out.println("修改");
         SpringApplication.run(CitestApplication.class, args);
     }
 
